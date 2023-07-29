a5850dcd3e359c879a76a4508846aaa8f31a9a02
Hardik Kanjariya <Hardik.Kanjariya@Kalelogistics.com>
2023-07-29 16:01:49 +0530

    update script.js

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function doSomething() { 
     // Do something 
 } 
+
+// Path: script.js 
+
+// Interesting Function 
+function interestingFunction() { 
+    // Simething interesting happens here 
+
+    // Call doSomething() 
+} 
